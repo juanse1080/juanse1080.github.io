8f4939be27407b7fbbd820c95b8d20602ddc9b92
Juan Sebastian <juanmarcon1080@gmail.com>
2020-11-20 20:12:04 -0500

    Add tooltip in button scoll back to top

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -3,6 +3,8 @@ import React from "react";
 // Import Material UI components
 import Zoom from "@material-ui/core/Zoom";
 import Fab from "@material-ui/core/Fab";
+import Tooltip from "@material-ui/core/Tooltip";
+
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 // import styles
@@ -13,16 +15,21 @@ export default function Footer({ hidden, scrollToTop, ...other }) {
 
    return (
       <Zoom in={hidden}>
-         <div role="presentation" className={classes.buttonScroll}>
-            <Fab
-               onClick={scrollToTop}
-               color="secondary"
-               size="small"
-               aria-label="scroll back to top"
-            >
-               <KeyboardArrowUpIcon />
-            </Fab>
-         </div>
+         <Tooltip
+            title="Scroll back to top"
+            placement="left"
+         >
+            <div role="presentation" className={classes.buttonScroll}>
+               <Fab
+                  onClick={scrollToTop}
+                  color="secondary"
+                  size="small"
+                  aria-label="scroll back to top"
+               >
+                  <KeyboardArrowUpIcon />
+               </Fab>
+            </div>
+         </Tooltip>
       </Zoom>
    );
 }
