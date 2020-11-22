39b5bbfd346283276772a968d922cfd42281a307
Juan Sebastian <juanmarcon1080@gmail.com>
2020-11-22 12:15:57 -0500

    Add subsection contact in About section

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -4,10 +4,17 @@ import React from "react";
 
 import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
+import GetAppIcon from "@material-ui/icons/GetApp";
 
 // Import local components
 import Image from "Components/Image";
+
+// import local assets
+import cv from "assets/docs/CV/JuanMarconCV.pdf";
 
 // import styles
 import useStyles from "./styles";
@@ -59,6 +66,53 @@ const About = ({ theme, photo, ...other }) => {
                   proyectos de desarrollo web durante el transcurso de mi
                   carrera profesional.
                </Typography>
+               <Typography variant="subtitle2" paragraph align="left">
+                  Contacto
+               </Typography>
+               <Grid
+                  container
+                  justify="space-between"
+                  alignItems="start"
+                  spacing={3}
+               >
+                  <Grid item>
+                     <Typography
+                        color="inherit"
+                        variant="body1"
+                        align="left"
+                        paragraph
+                     >
+                        <Link
+                           href="mailto:juanmarcon1080@gmail.com?Subject=Github%20page:%20"
+                           target="_blank"
+                           color="inherit"
+                           rel="noreferrer"
+                        >
+                           juanmarcon1080@gmail.com
+                        </Link>
+                     </Typography>
+                     <Typography color="inherit" variant="body1" align="left">
+                        <Link href="tel:3154390477" color="inherit">
+                           (+57) 3154390477
+                        </Link>
+                     </Typography>
+                  </Grid>
+                  <Grid item>
+                     <Link
+                        href={cv}
+                        color="inherit"
+                        download="JuanMarconCV.pdf"
+                     >
+                        <Button
+                           variant="contained"
+                           color="secondary"
+                           startIcon={<GetAppIcon />}
+                        >
+                           Descargar CV
+                        </Button>
+                     </Link>
+                  </Grid>
+               </Grid>
             </div>
          </Grid>
          <Hidden smDown>
--- src/Sections/About/styles.js
+++ src/Sections/About/styles.js
@@ -1,8 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-   avatar: {},
    content: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
       paddingRight: theme.spacing(3),
       [theme.breakpoints.down("sm")]: {
          padding: theme.spacing(0),
