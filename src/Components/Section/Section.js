87d2db88fdf73e19249c222b2c8e7b637cb8bc5b
Juan Sebastian <juanmarcon1080@gmail.com>
2020-11-30 12:53:46 -0500

    Delete Zoom efect in Section component

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -1,10 +1,10 @@
-import React, { forwardRef, useEffect, useState, useMemo } from "react";
+import React, { forwardRef } from "react";
 
 // Import Material UI components
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import Zoom from "@material-ui/core/Zoom";
+// import Zoom from "@material-ui/core/Zoom";
 
 // import styles
 import useStyles from "./styles";
@@ -27,49 +27,14 @@ const Section = forwardRef(
    ) => {
       const classes = useStyles({ theme });
 
-      const [initSection, setInitSection] = useState(false);
-
-      useMemo(() => {
-         if (loads.includes(section_name)) setInitSection(true);
-      }, [loads]);
-
       return (
          <div ref={ref}>
             <div className={classes.root}>
-               <Zoom in={initSection}>
-                  {fullWidth ? (
-                     <>
-                        <div className={classes.paddings}>
-                           <Container>
-                              <Grid container spacing={3} justify="flex-start">
-                                 <Grid item lg={3} md={3} sm={4} xs={12}>
-                                    <div className={classes.header}>
-                                       <Typography
-                                          className={classes.title}
-                                          color="inherit"
-                                          variant="h2"
-                                       >
-                                          {title}
-                                       </Typography>
-                                       <Typography
-                                          className={classes.subtitle}
-                                          color="inherit"
-                                          variant="subtitle1"
-                                       >
-                                          {subtitle}
-                                       </Typography>
-                                    </div>
-                                 </Grid>
-                              </Grid>
-                           </Container>
-                        </div>
-                        {children}
-                        <div className={classes.paddingBottom} />
-                     </>
-                  ) : (
+               {fullWidth ? (
+                  <>
                      <div className={classes.paddings}>
                         <Container>
-                           <Grid container spacing={3} justify="center">
+                           <Grid container spacing={3} justify="flex-start">
                               <Grid item lg={3} md={3} sm={4} xs={12}>
                                  <div className={classes.header}>
                                     <Typography
@@ -87,18 +52,43 @@ const Section = forwardRef(
                                        {subtitle}
                                     </Typography>
                                  </div>
-                                 {leftPart}
-                              </Grid>
-                              <Grid item lg={9} md={9} sm={8} xs={12}>
-                                 <div className={classes.content}>
-                                    {children}
-                                 </div>
                               </Grid>
                            </Grid>
                         </Container>
                      </div>
-                  )}
-               </Zoom>
+                     {children}
+                     <div className={classes.paddingBottom} />
+                  </>
+               ) : (
+                  <div className={classes.paddings}>
+                     <Container>
+                        <Grid container spacing={3} justify="center">
+                           <Grid item lg={3} md={3} sm={4} xs={12}>
+                              <div className={classes.header}>
+                                 <Typography
+                                    className={classes.title}
+                                    color="inherit"
+                                    variant="h2"
+                                 >
+                                    {title}
+                                 </Typography>
+                                 <Typography
+                                    className={classes.subtitle}
+                                    color="inherit"
+                                    variant="subtitle1"
+                                 >
+                                    {subtitle}
+                                 </Typography>
+                              </div>
+                              {leftPart}
+                           </Grid>
+                           <Grid item lg={9} md={9} sm={8} xs={12}>
+                              <div className={classes.content}>{children}</div>
+                           </Grid>
+                        </Grid>
+                     </Container>
+                  </div>
+               )}
             </div>
          </div>
       );
