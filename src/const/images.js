18778c318cd648372be251b2f7b992a240db18fe
Juan Sebastian <juanmarcon1080@gmail.com>
2020-12-01 18:12:16 -0500

    Fill Portfolio const

--- src/const/images.js
+++ src/const/images.js
@@ -1,7 +1,15 @@
+import vt_asesores from "assets/img/Portfolio/Asesores/Landing.png";
+import colcief from "assets/img/Portfolio/Colcief/Landing1.png";
+import bivlab from "assets/img/Portfolio/Bivl2ab/Landing.png";
+import crowd_investment from "assets/img/Portfolio/CrowdInvestment/Login.png";
+import plataforma_tic from "assets/img/Portfolio/PlataformaTIC/Login.png";
+import deep_tools from "assets/img/Portfolio/Deeptools/Login.png";
+
 const images = [
    {
-      img: "https://material-ui.com/static/images/grid-list/honey.jpg",
-      title: "honey",
+      key: "vt_asesores",
+      img: vt_asesores,
+      title: "Vt Asesores",
       author: "Author",
       featured: false,
       kind: [0, 1],
@@ -9,8 +17,9 @@ const images = [
       show: true,
    },
    {
-      img: "https://material-ui.com/static/images/grid-list/breakfast.jpg",
-      title: "breakfast",
+      key: "colcief",
+      img: colcief,
+      title: "ColCIEF",
       author: "Author",
       featured: false,
       kind: [0, 2],
@@ -18,8 +27,9 @@ const images = [
       show: true,
    },
    {
-      img: "https://material-ui.com/static/images/grid-list/burgers.jpg",
-      title: "burgers",
+      key: "bivlab",
+      img: bivlab,
+      title: "Bivl2ab",
       author: "Author",
       featured: false,
       kind: [0, 3],
@@ -27,8 +37,9 @@ const images = [
       show: true,
    },
    {
-      img: "https://material-ui.com/static/images/grid-list/camera.jpg",
-      title: "camera",
+      key: "crowd_investment",
+      img: crowd_investment,
+      title: "CrowdInvestment",
       author: "Author",
       featured: false,
       kind: [0, 4],
@@ -36,8 +47,9 @@ const images = [
       show: true,
    },
    {
-      img: "https://material-ui.com/static/images/grid-list/hats.jpg",
-      title: "hats",
+      key: "plataforma_tic",
+      img: plataforma_tic,
+      title: "Plataforma TIC",
       author: "Author",
       featured: false,
       kind: [0, 1],
@@ -45,67 +57,15 @@ const images = [
       show: true,
    },
    {
-      img: "https://material-ui.com/static/images/grid-list/morning.jpg",
-      title: "morning",
+      key: "deep_tools",
+      img: deep_tools,
+      title: "Deep tools",
       author: "Author",
       featured: true,
       kind: [0, 2],
-      timeout: 0,
-      show: true,
-   },
-   {
-      img: "https://material-ui.com/static/images/grid-list/mushroom.jpg",
-      title: "mushroom",
-      author: "Author",
-      featured: true,
-      kind: [0, 2],
-      timeout: 0,
-      show: true,
-   },
-   {
-      img: "https://material-ui.com/static/images/grid-list/olive.jpg",
-      title: "olive",
-      author: "Author",
-      featured: false,
-      kind: [0, 4],
-      timeout: 0,
-      show: true,
-   },
-   {
-      img: "https://material-ui.com/static/images/grid-list/plant.jpg",
-      title: "plant",
-      author: "Author",
-      featured: false,
-      kind: [0, 1],
-      timeout: 0,
-      show: true,
-   },
-   {
-      img: "https://material-ui.com/static/images/grid-list/star.jpg",
-      title: "star",
-      author: "Author",
-      featured: false,
-      kind: [0, 2],
-      timeout: 0,
-      show: true,
-   },
-   {
-      img: "https://material-ui.com/static/images/grid-list/vegetables.jpg",
-      title: "vegetables",
-      author: "Author",
-      featured: false,
-      kind: [0, 3],
       timeout: 0,
       show: true,
    },
 ];
 
-const images_kinds = {
-   0: "Todas",
-   1: "PHP",
-   2: "Python",
-   3: "React JS",
-   4: "Django",
-};
-
-export { images, images_kinds };
+export default images;
