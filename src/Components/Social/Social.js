58204aba65836a7350043b62223118418960d818
Juan Sebastian <juanmarcon1080@gmail.com>
2020-12-01 09:18:17 -0500

    Fix key bug in Social Component

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -6,7 +6,7 @@ import socials from "const/socials";
 // import styles
 import useStyles from "./styles";
 
-export default function Legend({ theme, mailAction, ...others }) {
+export default function Social({ theme, mailAction, ...others }) {
    const classes = useStyles({ theme });
    return (
       <div {...others}>
@@ -17,7 +17,7 @@ export default function Legend({ theme, mailAction, ...others }) {
                name,
                props,
             }) => (
-               <Component {...propsChildren}>
+               <Component key={name} {...propsChildren}>
                   <Icon
                      {...props}
                      fontSize="inherit"
