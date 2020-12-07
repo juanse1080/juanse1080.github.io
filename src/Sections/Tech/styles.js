40ba86049f2ff940725c9280be8239b1b8462429
Juan Sebastian <juanmarcon1080@gmail.com>
2020-12-07 10:52:39 -0500

    Animation in hover in Tech section

--- src/Sections/Tech/styles.js
+++ src/Sections/Tech/styles.js
@@ -12,12 +12,12 @@ const useStyles = makeStyles(theme => ({
    },
    icon: {
       cursor: "pointer",
-      transition: "all 0.4s ease-in-out",
-      transform: "translateY(-1px)",
-      filter: "drop-shadow(0px 1px 0px rgba(0,0,0,0.6))",
+      transition: "all 0.3s ease-in-out",
+      transform: "translateY(-3px)",
+      filter: "drop-shadow(0px 3px 0px rgba(0,0,0,0.6))",
       "&:hover": {
-         transform: "translateY(-9px)",
-         filter: "drop-shadow(0px 9px 0px rgba(0,0,0,0.6))",
+         transform: "translateY(-11px)",
+         filter: "drop-shadow(0px 11px 0px rgba(0,0,0,0.6))",
       },
    },
    title: {
