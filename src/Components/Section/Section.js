2f09cb2c647a28c2d72d8d6fb83ca40448a1974c
Juan Sebastian <juanmarcon1080@gmail.com>
2020-12-09 14:43:47 -0500

    Add listener function to change section

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -6,6 +6,8 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 // import Zoom from "@material-ui/core/Zoom";
 
+import { Waypoint } from "react-waypoint";
+
 // import styles
 import useStyles from "./styles";
 
@@ -18,18 +20,59 @@ const Section = ({
    theme,
    children,
    fullWidth,
+   handlePage,
+   before,
+   after,
    ...other
 }) => {
    const classes = useStyles({ theme });
 
+   const onLeave = ({ currentPosition, previousPosition }) => {
+      if (currentPosition === "above" && previousPosition === "inside" && after)
+         handlePage(after);
+   };
+
+   const onEnter = ({ currentPosition, previousPosition }) => {
+      if (currentPosition === "inside" && previousPosition === "above" && after)
+         handlePage(id);
+   };
+
    return (
-      <div id={id} {...other}>
-         <div className={classes.root}>
-            {fullWidth ? (
-               <>
+      <Waypoint topOffset={64} onLeave={onLeave} onEnter={onEnter}>
+         <div id={id} {...other}>
+            <div className={classes.root}>
+               {fullWidth ? (
+                  <>
+                     <div className={classes.paddings}>
+                        <Container>
+                           <Grid container spacing={3} justify="flex-start">
+                              <Grid item lg={3} md={3} sm={4} xs={12}>
+                                 <div className={classes.header}>
+                                    <Typography
+                                       className={classes.title}
+                                       color="inherit"
+                                       variant="h2"
+                                    >
+                                       {title}
+                                    </Typography>
+                                    <Typography
+                                       className={classes.subtitle}
+                                       color="inherit"
+                                       variant="subtitle1"
+                                    >
+                                       {subtitle}
+                                    </Typography>
+                                 </div>
+                              </Grid>
+                           </Grid>
+                        </Container>
+                     </div>
+                     {children}
+                  </>
+               ) : (
                   <div className={classes.paddings}>
                      <Container>
-                        <Grid container spacing={3} justify="flex-start">
+                        <Grid container spacing={3} justify="center">
                            <Grid item lg={3} md={3} sm={4} xs={12}>
                               <div className={classes.header}>
                                  <Typography
@@ -47,44 +90,18 @@ const Section = ({
                                     {subtitle}
                                  </Typography>
                               </div>
+                              {leftPart}
+                           </Grid>
+                           <Grid item lg={9} md={9} sm={8} xs={12}>
+                              <div className={classes.content}>{children}</div>
                            </Grid>
                         </Grid>
                      </Container>
                   </div>
-                  {children}
-               </>
-            ) : (
-               <div className={classes.paddings}>
-                  <Container>
-                     <Grid container spacing={3} justify="center">
-                        <Grid item lg={3} md={3} sm={4} xs={12}>
-                           <div className={classes.header}>
-                              <Typography
-                                 className={classes.title}
-                                 color="inherit"
-                                 variant="h2"
-                              >
-                                 {title}
-                              </Typography>
-                              <Typography
-                                 className={classes.subtitle}
-                                 color="inherit"
-                                 variant="subtitle1"
-                              >
-                                 {subtitle}
-                              </Typography>
-                           </div>
-                           {leftPart}
-                        </Grid>
-                        <Grid item lg={9} md={9} sm={8} xs={12}>
-                           <div className={classes.content}>{children}</div>
-                        </Grid>
-                     </Grid>
-                  </Container>
-               </div>
-            )}
+               )}
+            </div>
          </div>
-      </div>
+      </Waypoint>
    );
 };
 
