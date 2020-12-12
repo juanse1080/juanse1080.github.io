91bf3378e82629d1a4437af733b5493670bb30c0
Juan Sebastian <juanmarcon1080@gmail.com>
2020-12-12 08:27:54 -0500

    Adding media in project Section

---
+++ src/const/deeptools.js
@@ -0,0 +1,65 @@
+import ALgorithm from "assets/img/Portfolio/Deeptools/ALgorithm.png";
+import Experiment from "assets/img/Portfolio/Deeptools/Experiment.png";
+import newAlgorithm from "assets/img/Portfolio/Deeptools/newAlgorithm.png";
+import Run from "assets/img/Portfolio/Deeptools/Run.mp4";
+import Algorithms from "assets/img/Portfolio/Deeptools/Algorithms.png";
+import Login from "assets/img/Portfolio/Deeptools/Login.png";
+import Notifications from "assets/img/Portfolio/Deeptools/Notifications.png";
+import Running from "assets/img/Portfolio/Deeptools/Running.png";
+import Dashboard from "assets/img/Portfolio/Deeptools/Dashboard.png";
+import makeExperiment from "assets/img/Portfolio/Deeptools/makeExperiment.png";
+import Profile from "assets/img/Portfolio/Deeptools/Profile.png";
+import Tests from "assets/img/Portfolio/Deeptools/Tests.png";
+
+const images = [
+   {
+      url: Login,
+      type: "image",
+   },
+   {
+      url: ALgorithm,
+      type: "image",
+   },
+   {
+      url: Experiment,
+      type: "image",
+   },
+   {
+      url: newAlgorithm,
+      type: "image",
+   },
+   {
+      url: Run,
+      type: "video",
+   },
+   {
+      url: Algorithms,
+      type: "image",
+   },   
+   {
+      url: Notifications,
+      type: "image",
+   },
+   {
+      url: Running,
+      type: "image",
+   },
+   {
+      url: Dashboard,
+      type: "image",
+   },
+   {
+      url: makeExperiment,
+      type: "image",
+   },
+   {
+      url: Profile,
+      type: "image",
+   },
+   {
+      url: Tests,
+      type: "image",
+   },
+];
+
+export default images;
