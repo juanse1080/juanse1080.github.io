d58ca9a7b1329401c3ca37bd9c431286b72d343f
juanse1080 <juanmarcon1080@gmail.com>
2021-01-25 13:13:20 -0500

    Updating experience

--- src/const/experience.js
+++ src/const/experience.js
@@ -1,70 +1,92 @@
 const experience = {
-   espanol: [
-      {
-         title: "AUXILIAR DOCENTE",
-         date: "Enero 2019 - Mayo 2019",
-         institution: "UIS",
-         description: [
-            "Contribución al desarrollo de una plataforma Navbar-PSI, una pagina para la gestión de prótesis a medida.",
-         ],
-         ended: true,
-         techs: ["Python 3.5", "Django 2.1", "Javascript", "React JS", "MySQL"],
-      },
-      {
-         title: "AUXILIAR ADMINISTRATIVO",
-         date: "Febrero 2019 - Octubre 2020",
-         institution: "IPRED",
-         description: [
-            "Al iniciar mi instancia con el Instituto de Proyección Regional y Educación a Distancia (IPRED) comencé como desarrollador fullStack, empezando con la adaptación del software para la migración de la base de datos. Ademas se desarrollaron multiples vistas administrativas para el manejo de la información de los estudiantes y profesores.",
-            "Pasado el tiempo se me asigno la optimización del algoritmo de generación de horarios y la automatización del despliegue de los horarios.",
-         ],
-         ended: true,
-         techs: [
-            "PHP 7",
-            "PostgreSQL",
-            "JQuery",
-            "Javascript",
-            "Bootstrap 4",
-            "React JS",
-            "Flask",
-            "Python 3.7",
-            "gRPC",
-         ],
-      },
-   ],
-   english: [
-      {
-         title: "TEACHING ASSISTANT",
-         date: "January 2019 - May 2019",
-         institution: "UIS",
-         description: [
-            "Contribution to the development of a Navbar-PSI platform, a page for the management of custom prostheses.",
-         ],
-         ended: true,
-         techs: ["Python 3.5", "Django 2.1", "Javascript", "React JS", "MySQL"],
-      },
-      {
-         title: "ADMINISTRATIVE ASSISTANT",
-         date: "February 2019 - October 2020",
-         institution: "IPRED",
-         description: [
-            "When I started my instance with the Institute for Regional Projection and Distance Education (IPRED) I started as a fullStack developer, starting with the adaptation of the software for the migration of the database. In addition, multiple administrative views were developed for the management of student and teacher information",
-            "After time, I was assigned the optimization of the timetable generation algorithm and the automation of the timetable deployment."
-         ],
-         ended: true,
-         techs: [
-            "PHP 7",
-            "PostgreSQL",
-            "JQuery",
-            "Javascript",
-            "Bootstrap 4",
-            "React JS",
-            "Flask",
-            "Python 3.7",
-            "gRPC",
-         ],
-      },
-   ], 
+  espanol: [
+    {
+      title: "DESARROLLADOR FRONTEND",
+      date: "Enero 7",
+      institution: "Binary Groups",
+      description: [
+        "Al iniciar mi instancia con el Instituto de Proyección Regional y Educación a Distancia (IPRED) comencé como desarrollador fullStack, empezando con la adaptación del software para la migración de la base de datos. Ademas se desarrollaron multiples vistas administrativas para el manejo de la información de los estudiantes y profesores.",
+        "Pasado el tiempo se me asigno la optimización del algoritmo de generación de horarios y la automatización del despliegue de los horarios.",
+      ],
+      ended: false,
+      techs: ["React JS", "Material UI"],
+    },
+    {
+      title: "AUXILIAR ADMINISTRATIVO",
+      date: "Febrero 2019 - Octubre 2020",
+      institution: "IPRED",
+      description: [
+        "Al iniciar mi instancia con el Instituto de Proyección Regional y Educación a Distancia (IPRED) comencé como desarrollador fullStack, empezando con la adaptación del software para la migración de la base de datos. Ademas se desarrollaron multiples vistas administrativas para el manejo de la información de los estudiantes y profesores.",
+        "Pasado el tiempo se me asigno la optimización del algoritmo de generación de horarios y la automatización del despliegue de los horarios.",
+      ],
+      ended: true,
+      techs: [
+        "PHP 7",
+        "PostgreSQL",
+        "JQuery",
+        "Javascript",
+        "Bootstrap 4",
+        "React JS",
+        "Flask",
+        "Python 3.7",
+        "gRPC",
+      ],
+    },
+    {
+      title: "AUXILIAR DOCENTE",
+      date: "Enero 2019 - Mayo 2019",
+      institution: "UIS",
+      description: [
+        "Contribución al desarrollo de una plataforma Navbar-PSI, una pagina para la gestión de prótesis a medida.",
+      ],
+      ended: true,
+      techs: ["Python 3.5", "Django 2.1", "Javascript", "React JS", "MySQL"],
+    },
+  ],
+  english: [
+    {
+      title: "FRONTEND DEVELOPER",
+      date: "January 7",
+      institution: "Binary Groups",
+      description: [
+        "When I started my instance with the Institute for Regional Projection and Distance Education (IPRED) I started as a fullStack developer, starting with the adaptation of the software for the migration of the database. In addition, multiple administrative views were developed for the management of student and teacher information",
+        "After time, I was assigned the optimization of the timetable generation algorithm and the automation of the timetable deployment.",
+      ],
+      ended: false,
+      techs: ["React JS", "Material UI"],
+    },
+    {
+      title: "ADMINISTRATIVE ASSISTANT",
+      date: "February 2019 - October 2020",
+      institution: "IPRED",
+      description: [
+        "When I started my instance with the Institute for Regional Projection and Distance Education (IPRED) I started as a fullStack developer, starting with the adaptation of the software for the migration of the database. In addition, multiple administrative views were developed for the management of student and teacher information",
+        "After time, I was assigned the optimization of the timetable generation algorithm and the automation of the timetable deployment.",
+      ],
+      ended: true,
+      techs: [
+        "PHP 7",
+        "PostgreSQL",
+        "JQuery",
+        "Javascript",
+        "Bootstrap 4",
+        "React JS",
+        "Flask",
+        "Python 3.7",
+        "gRPC",
+      ],
+    },
+    {
+      title: "TEACHING ASSISTANT",
+      date: "January 2019 - May 2019",
+      institution: "UIS",
+      description: [
+        "Contribution to the development of a Navbar-PSI platform, a page for the management of custom prostheses.",
+      ],
+      ended: true,
+      techs: ["Python 3.5", "Django 2.1", "Javascript", "React JS", "MySQL"],
+    },
+  ],
 };
 
 export default experience;
